f1abc7fd4d13c77233c8f980b476f43a15a044fc
Aslam Sarfraz <aslam91r@gmail.com>
2021-01-02 00:39:28 +0500

    Complete LED-Roulette

--- LED-Roulette/src/main.rs
+++ LED-Roulette/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+
+#![no_main]
+#![no_std]
+
+use aux::{entry, Delay,Leds,prelude::*}
+
+![entry]
+
+fn main(){
+    let (delay,leds): Delay, Leds = aux::init();
+    let period = 100_u16;
+
+    loop{
+        leds[0].on();
+        delay.delay_ms(period);
+        leds[0].off();
+
+        leds[2].on();
+        delay.delay_ms(period);
+        leds[2].off();
+
+        leds[4].on();
+        delay.delay_ms(period);
+        leds[4].off();
+    
+        leds[6].on();
+        delay.delay_ms(period);
+        leds[6].off();
+    }
 }
